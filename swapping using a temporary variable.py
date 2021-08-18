780d91cd3ea4f4b9e40edcddb72201155e7ef797
lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-18 21:34:54 +0530

    Add files via upload

---
+++ swapping using a temporary variable.py
@@ -0,0 +1,9 @@
+#python script for swapping of two numbers
+def swap():
+    a=int(input('enter a number:'))
+    b=int(input('enter a number:'))
+    c=a
+    a=b
+    b=c
+    print("the values of a and b after swapping is","a=",a,"b=",b)
+    
